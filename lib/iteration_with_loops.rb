5b4d207f6ed6ca95ab501fb805fc1e0434574304
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-19 09:42:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,11 +12,11 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a? String
         array_of_strings.push(src[row_index][element_index])
-       
+        element_index += 1
       end
-      # element_index += 1
+      
     end
-    # row_index += 1
+    row_index += 1
   end
   joined_strings = array_of_strings.join(" ")
   
