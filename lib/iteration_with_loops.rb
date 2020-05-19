19dcf25c99ff14e6b336b449d3a0dbc5a3cc15db
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-19 08:54:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,8 +9,8 @@ def join_nested_strings(src)
     inner_results = []
     joined_string = ""
     while element_index < src[row_index].count do
-      if src[row_index][element_index].is_s?
-        lowest_value = src[row_index][element_index]
+      if src[row_index][element_index].is_a? String
+        joined.string("")
       end
       element_index += 1
     end
